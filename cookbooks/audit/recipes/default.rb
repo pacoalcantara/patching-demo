581f642179891dff6f561fb852979e550fd8d66b
pacoalcantara <alcantaranet@gmail.com>
2016-02-12 22:28:00 +0900

    Added controls to check the firewall

--- cookbooks/audit/recipes/default.rb
+++ cookbooks/audit/recipes/default.rb
@@ -13,3 +13,32 @@ control_group 'Validate web services' do
     }
   end
 end
+
+control_group 'Validate network configuration and firewalls' do
+  control 'Ensure the firewall is active' do
+    it 'enables the iptables service' do
+      expect(service('iptables')).to be_enabled
+    end
+
+    it 'has iptables running' do
+      expect(service('iptables')).to be_running
+    end
+
+    it 'accepts SSH connections' do
+      expect(iptables).to have_rule('-A INPUT -i eth0 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT')
+    end
+
+    it 'accepts HTTP connections' do
+      expect(iptables).to have_rule('-A INPUT -i eth0 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT')
+    end
+
+    it 'rejects all other connections' do
+      expect(iptables).to have_rule('-P INPUT DROP')
+      expect(iptables).to have_rule('-P FORWARD DROP')
+    end
+
+    it 'permits all outbound traffic' do
+      expect(iptables).to have_rule('-P OUTPUT ACCEPT')
+    end
+  end
+end
