310b199561e89c77f8b0b46cc75a9b734ad76cdb
pacoalcantara <alcantaranet@gmail.com>
2016-02-17 18:20:06 +0900

    Updated versions

--- cookbooks/webserver/metadata.rb
+++ cookbooks/webserver/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures webserver'
 long_description 'Installs/Configures webserver'
-version '0.2.0'
+version '0.2.1'
 
 # Compliance Demo code - Part 2
 depends 'iptables', '~> 1.0.0'
