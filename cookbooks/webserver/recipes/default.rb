b76d0c76ec7f73992de4a7ac1b3264dd57222ec5
pacoalcantara <alcantaranet@gmail.com>
2016-02-25 16:28:11 +0900

    Changes to html file

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -39,7 +39,7 @@ end
 # Add files to the site.
 %w(index.html pages/page1.html pages/page2.html).each do |web_file|
   file File.join('/var/www/html', web_file) do
-    content "<html> <body> <h1> This is file <b>#{web_file}</b> - Hello World! </h1> </body> </html>"
+    content "<html> <body> <h1> This is file [#{web_file}] on <%= node['fqdn'] %> - Hello World! </h1> </body> </html>"
     group 'web_admin'
     user 'web_admin'
   end
