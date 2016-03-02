63ca659515f9a8625a4e048c048bae1578ce03ed
pacoalcantara <alcantaranet@gmail.com>
2016-03-02 11:25:51 +0900

    Firewall off

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -39,7 +39,7 @@ end
 # Add files to the site.
 %w(index.html pages/page1.html pages/page2.html).each do |web_file|
   file File.join('/var/www/html', web_file) do
-    content "<html> <body> <h1> これは [#{web_file}]ファイルです - ハローワールド! </h1> </body> </html>"
+    content "<html> <body> <h1> This is file [#{web_file}] Hello World! </h1> </body> </html>"
     group 'web_admin'
     user 'web_admin'
   end
