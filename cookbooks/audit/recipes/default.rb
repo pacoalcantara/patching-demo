a8c212496dd1a9e2e42d1e7c9394acf03d7a1fc3
Francisco Alcantara <francisco.alcantara@hpe.com>
2016-06-02 18:43:30 +0900

    Added compliance tests

--- cookbooks/audit/recipes/default.rb
+++ cookbooks/audit/recipes/default.rb
@@ -6,7 +6,7 @@
 
 # =========================================
 # Test for PACKAGE versions
-=begin
+
 # Test for version of BASH
 #if defined? node[:packages][:openssl][:version] && node[:packages][:openssl][:version] < node[:openssl][:req_version]
 control_group 'BASH Audit' do
@@ -35,7 +35,7 @@ control_group 'OPENSSL Audit' do
     end
   end
 end
-=end
+
 # =========================================
 # Test for OS security
 
