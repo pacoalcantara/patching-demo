cece21f6edabebf348ec20c97611b7a7a64923b0
Francisco Alcantara <francisco.alcantara@hpe.com>
2016-06-07 14:00:15 +0900

    Patchy time

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -14,14 +14,14 @@ iptables_rule 'firewall'
 # Compliance Demo code - Part 2
 
 # Install the httpd package.
-package 'httpd'
-=begin
+#package 'httpd'
+
 %w{ bash httpd openssl }.each do |pkg|
   package "#{pkg}" do
     action [ :install, :upgrade ]
   end
 end
-=end
+
 
 # Enable and start the httpd service.
 service 'httpd' do
